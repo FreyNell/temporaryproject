89873e44c0298f8d52cfe7533796581fdf06b21f
freynell <freybalaguera@gmail.com>
2022-05-03 23:02:04 -0500

    install

--- install.sh
+++ install.sh
@@ -1,11 +1,3 @@
-nohup docker-compose down &
-sleep 10
-nohup docker-compose up &
-sleep 10
-nohup docker-compose run web python backend/manage.py makemigrations &
-sleep 5
-nohup docker-compose run web python backend/manage.py migrate &
-sleep 5
-nohup docker-compose run web python backend/manage.py loaddata db.json &
-sleep 5
-echo "Finished"
+docker-compose run web python backend/manage.py makemigrations
+docker-compose run web python backend/manage.py migrate
+docker-compose run web python backend/manage.py loaddata db.json
